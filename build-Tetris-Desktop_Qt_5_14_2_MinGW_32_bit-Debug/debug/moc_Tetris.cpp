6689272ccf2736c913adbc667d34ec54b1ea9cdb
CodingPoetss <2022212515sjw@bupt.edu.cn>
2023-07-12 09:44:04 +0800

    Delete build-Tetris-Desktop_Qt_5_14_2_MinGW_32_bit-Debug directory

--- build-Tetris-Desktop_Qt_5_14_2_MinGW_32_bit-Debug/debug/moc_Tetris.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:76158bf522a6e7b52bbf9a98c6d7351cf1ed86bcba13374f3e1dea9207375c91
-size 2721
--- build-Tetris-Desktop_Qt_5_14_2_MinGW_32_bit-Debug/debug/qrc_Tetris.cpp
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:65029ab2f472fca94302d0b534b6c6a1affc2192990b063182f11455d12d0670
-size 1391
